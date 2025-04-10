08903078bf0e5fd03dfc121eea654c2effe151e1
bybeejac000 <jake.bybee@bybeemail.com>
2025-04-10 12:09:04 -0600

    trigger

--- frontend/src/pages/RegistrationPage.tsx
+++ frontend/src/pages/RegistrationPage.tsx
@@ -376,7 +376,7 @@ function RegistrationPage() {
             </button>
           </div>
         );
-      // Step 9: verification codes
+      // Step 9: verification code
       case 9:
         return (
           <div className="mb-3 text-center">
